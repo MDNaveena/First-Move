7a4e9f0d622993dd05cf28a268248d5ec0eec720
Naveena@196 <naveenamd111@gmail.com>
2021-11-26 22:28:12 +0530

    Automation Tester 26/11/2021

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,35 @@
+package org.add;
+
+public class GreensTech {
+	
+	private void greensOmr() {
+		System.out.println("A");
+
+	}
+	private void greensAdayar() {
+		System.out.println("B");
+
+	}
+	private void greensTambaram() {
+		System.out.println("C");
+
+	}
+	private void greensVelacherry() {
+		System.out.println("D");
+
+	}
+	private void greensAnnaNagar() {
+		System.out.println("E");
+
+	}
+	public static void main(String[] args) {
+		GreensTech jrd = new GreensTech();
+		jrd.greensOmr();
+		jrd.greensAdayar();
+		jrd.greensTambaram();
+		jrd.greensVelacherry();
+		jrd.greensAnnaNagar();
+		
+	}
+
+}
